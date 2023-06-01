dd20bb00f9f6231ee8eb70dcb551141e50f8d2a2
Bruno da Silva Barcelos <bruno.barcelos@meta.com.br>
2023-06-01 15:57:16 -0300

    implementando a listagem dos medicos com paginação definindo 10 registros por pagina e ordenados

--- src/main/java/med/voll/API/controller/MedicoController.java
+++ src/main/java/med/voll/API/controller/MedicoController.java
@@ -3,13 +3,17 @@ package med.voll.API.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.API.medico.DadosCadastroMedico;
+import med.voll.API.medico.DadosListagemMedico;
 import med.voll.API.medico.Medico;
 import med.voll.API.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -23,4 +27,15 @@ public class MedicoController {
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
+
+//    @GetMapping
+//    public List<DadosListagemMedico> listar() {
+//        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+//    }
+    // listagem dos medicos com paginação
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size=10, sort = {"nome"}, direction = Sort.Direction.DESC) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
+
 }
