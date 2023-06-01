fa850b63c516b9c060480b8c5c4abf73aba026e5
Bruno da Silva Barcelos <bruno.barcelos@meta.com.br>
2023-06-01 13:20:30 -0300

    implementando as classes de persistencia do cliente

--- src/main/java/med/voll/API/controller/PacienteController.java
+++ src/main/java/med/voll/API/controller/PacienteController.java
@@ -1,14 +1,19 @@
 package med.voll.API.controller;
 
 import med.voll.API.paciente.DadosCadastroPaciente;
+import med.voll.API.paciente.Paciente;
+import med.voll.API.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
 public class PacienteController {
 
+    @Autowired
+    private PacienteRepository repository;
     @PostMapping
     public void cadastrarPaciente(@RequestBody DadosCadastroPaciente dados) {
-        System.out.println(dados);
+        repository.save(new Paciente(dados));
     }
 }
--- src/main/java/med/voll/API/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/voll/API/paciente/DadosCadastroPaciente.java
@@ -2,5 +2,10 @@ package med.voll.API.paciente;
 
 import med.voll.API.endereco.Endereco;
 
-public record DadosCadastroPaciente(String nome, String email, String telefone, String CPF, Endereco endereco ) {
+public record DadosCadastroPaciente(
+        String nome,
+        String email,
+        String telefone,
+        String cpf,
+        Endereco endereco ) {
 }
