ccf1ba799477e84de23764a7b629665f60b3a803
Bruno da Silva Barcelos <bruno.barcelos@meta.com.br>
2023-06-01 16:14:27 -0300

    implementando a listagem dos pacientes com paginação trazendo por padrao 10 registros ordenados por nome

--- src/main/java/med/voll/API/controller/PacienteController.java
+++ src/main/java/med/voll/API/controller/PacienteController.java
@@ -3,10 +3,16 @@ package med.voll.API.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.API.paciente.DadosCadastroPaciente;
+import med.voll.API.paciente.DadosListagemPaciente;
 import med.voll.API.paciente.Paciente;
 import med.voll.API.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -18,5 +24,9 @@ public class PacienteController {
     @Transactional
     public void cadastrarPaciente(@RequestBody @Valid DadosCadastroPaciente dados) {
         repository.save(new Paciente(dados));
+    }
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(sort = {"nome"}, size = 2) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 }
